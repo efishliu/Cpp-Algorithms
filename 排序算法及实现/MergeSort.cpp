5165a65a51d3f8107cbf3c3f63c79119cfa01dde
efish <liu920848992@163.com>
2020-01-14 16:24:37 +0800

    Update MergeSort.cpp

--- 排序算法及实现/MergeSort.cpp
+++ 排序算法及实现/MergeSort.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+void merge_t(T arr[],int left,int mid,int right);
+
+template <typename T>
+void mergeSort_t(T arr[],int left,int right){
+    if(left >= right)   return;
+    int mid = (left + right) / 2;
+    mergeSort_t(arr,left,mid);
+    mergeSort_t(arr,mid+1,right);
+    merge_t(arr,left,mid,right);
+}
+
+template <typename T>
+void merge_t(T arr[],int left,int mid,int right){
+    T *temp = new T[right - left + 1];
+    
+    for(int i = left;i <= right;i++)
+        temp[i - left] = arr[i];
+    
+    int i = left,j = mid + 1;
+    for(int k = left;k <= right;k++){
+        if(i > mid){
+            arr[k] = temp[j - left];
+            j++;
+        }
+        else if(j > right){
+            arr[k] = temp[i - left];
+            i++;
+        }
+        else if(arr[i] < arr[j]){
+            arr[k] = temp[i - left];
+            i++;
+        }
+        else{
+            arr[k] = temp[j - left];
+            j++;
+        }
+    
+    }
+}
+
+template <typename T>
+void mergeSort(T arr[],int n){
+    mergeSort_t(arr,0,n-1);
+}
+
+
 
